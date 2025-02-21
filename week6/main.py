a2841a73139b405c26d828869ae69753f1c8f5dc
chaohungwu <43366117+chaohungwu@users.noreply.github.com>
2025-02-21 15:52:47 +0800

    Add files via upload

--- week6/main.py
+++ week6/main.py
@@ -15,7 +15,6 @@ website_db = mysql.connector.connect(
                                     database="website",
                                     charset="utf8mb4")
 
-# print("DB ready")
 
 templates = Jinja2Templates(directory="templates")  # 指定模板目錄
 app = FastAPI()
@@ -31,35 +30,43 @@ app.add_middleware(SessionMiddleware, secret_key="YOUR_SECRET_KEY")#隨機產生
 def sigup_Verification(request: Request, body = Body(None)): #greater than or equal to 1
     data = json.loads(body)
     cursor = website_db.cursor()
-    cursor.execute("select username from member;")#選所有的帳號
+    """
+    這邊修改成先用資料庫做篩選
+    如果回傳值是null就可以註冊
+    回傳有值的話就不能註冊
+
+    這邊前端傳過來的資料為 名稱、帳號、密碼、
+    """
+
+    ##新的註冊姓名
+    new_username = data["signup_account"]
+
+    #查這個帳號有沒有在資料庫中
+    cursor.execute("select username from member where username= %s;", (new_username,))
     myresult = cursor.fetchall()#回傳所有資料庫指令結果
-    all_db_username_list = []
-
-    for (x,) in myresult: #因為取出值會有(,)所以要用for (row,) in items:的'方法避免
-        all_db_username_list.append(x)
-
-    new_username = data["signup_account"] 
-
-    print(data)
+    # print("123",myresult)
+    # print(myresult[0])
+    
 
     #如果在資料庫中
-    if new_username in all_db_username_list:
+    if len(myresult) != 0:
         message = "帳號重複"
+        # print(message)
         return {"sign-up": False, "message": message}
-    
-    else:#新增新的帳號至資料庫中
+
+    #新增新的帳號至資料庫中
+    else:
+        print("ok")
         cursor = website_db.cursor()
         new_signup_name = data["signup_name"]
         new_signup_account = data["signup_account"]
         new_signup_password = data["signup_password"]
-
-        cursor.execute("insert into member (name,username,password) values ('{}','{}','{}');".format(new_signup_name, new_signup_account, new_signup_password))#選所有的帳號
+        new_signu_data = (new_signup_name,new_signup_account,new_signup_password)
+        
+        #插入新的資料到資料庫
+        cursor.execute("insert into member (name,username,password) values (%s,%s,%s);", new_signu_data)
         website_db.commit()
         return {"sign-up": True, "message": "SUCCESS!"}
-
-
-
-
 
 
 
@@ -77,13 +84,16 @@ def sigin_Verification(request: Request, body = Body(None)):
     password = data["signin_password"]#從前端傳來的輸入密碼
 
     cursor = website_db.cursor()
-    cursor.execute("select name, username, password ,id from member where username = '{}';".format(account))#選所有的帳號
-
+    cursor.execute("select name, username, password ,id from member where username = %s;", (account,))#選所有符合的帳號
     myresult = cursor.fetchall()#回傳所有資料庫指令結果
+
+
     """
     這邊有2個可能
     1. 沒有該帳號、2. 密碼錯誤
     """
+
+
     if len(myresult)==0:
         return {"sign-in": False, "message": "帳號或密碼輸入錯誤"}
 
@@ -91,7 +101,9 @@ def sigin_Verification(request: Request, body = Body(None)):
         return {"sign-in": False, "message": "帳號或密碼輸入錯誤"}
 
     elif myresult[0][1] == account and myresult[0][2] == password:
-        request.session["user_id"] = account #將session存入request中
+        
+        request.session["user_id"] = myresult[0][1] #將session存入request中
+        request.session["user_id_index"] = myresult[0][3] #將session存入request中
 
         name = myresult[0][0]#名稱
         name_id = myresult[0][3]#名稱id
@@ -100,6 +112,8 @@ def sigin_Verification(request: Request, body = Body(None)):
 
     else:
         return {"sign-in": False, "message": "意外錯誤"}
+
+
 
 #登出(清除session)
 @app.get("/signout")
@@ -131,44 +145,45 @@ def error(request: Request, message: str):
 @app.post("/createMessage")
 def createMessage(request: Request, body = Body(None)):
     data = json.loads(body)
-    print(data)
+    # print(data)
 
-    # 空值的話{'new_message': ''}
+    name = request.session["user_id"]
+    name_id = request.session["user_id_index"]
+    
+    new_message = data["new_message"]
+
+    print(data["new_message"])
+
+    # 空值的話{'new_message': ''，重傳所有的留言}
     if data["new_message"] == "":
+        print(data["new_message"])
+
         cursor = website_db.cursor()
         cursor.execute("select member.name, message.content, message.id, member.id from message inner join member on message.member_id = member.id order by message.time desc;")#選所有的留言
-        
         myresult = cursor.fetchall()#回傳所有資料庫指令結果
-        print(myresult)
         
-        return{"new_message":False,"message": myresult}
+        return{"new_message":False,"message": myresult,"siginin_id":name_id}
 
 
     #有新留言值的話就先新增留言然後回傳所有的留言
-    #依照傳回來的名子做id查詢
-
+    #依照session裡儲存的帳號做id查詢
     else:
         cursor = website_db.cursor()
         #查member_id
-        name = data["name"]
-        new_message = data["new_message"]
         cursor.execute("select id from member where member.name = %s", (name,))
 
         myresult = cursor.fetchall()#回傳所有資料庫指令結果
-        user_id = myresult[0][0]#member_id(str)
-
-        print(user_id)
-        print(new_message)
-        print(type(new_message))
+        # user_id = myresult[0][0]#member_id(str)
 
         #新增留言
-        cursor.execute("insert into message (member_id, content) values(%s,%s)", (user_id, new_message))
+        cursor.execute("insert into message (member_id, content) values(%s,%s)", (name_id, new_message))
         website_db.commit()
         cursor.execute("select member.name, message.content, message.id, member.id from message inner join member on message.member_id = member.id order by message.time desc;")#選所有的留言
         myresult = cursor.fetchall()#回傳所有資料庫指令結果
 
-        
-        return{"new_message":True,"message": myresult}
+
+        print("aaaa",name)
+        return{"new_message":True,"message": myresult, "siginin_id":name_id}
 
 
 
@@ -179,7 +194,7 @@ def createMessage(request: Request, body = Body(None)):
     # print(data)
     # data2 = data
     # print(data["message_index"])
-
+    name_id = request.session["user_id_index"]
     cursor = website_db.cursor()
     cursor.execute("delete from message where message.id = %s",(data["message_index"],))
     website_db.commit()
@@ -187,12 +202,8 @@ def createMessage(request: Request, body = Body(None)):
     cursor.execute("select member.name, message.content, message.id, member.id from message inner join member on message.member_id = member.id order by message.time desc;")#選所有的留言
     myresult = cursor.fetchall()#回傳所有資料庫指令結果
 
-    return{"new_message":False,"message": myresult}
 
-
-
-
-
+    return{"new_message":False,"message": myresult, "siginin_id":name_id}
 
 
 
